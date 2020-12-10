95e6d032e6fe0c8d587be6807ad0178a50bdd994
Rus-tam <56486665+Rus-tam@users.noreply.github.com>
2020-12-10 17:01:31 +0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -22,7 +22,7 @@ const removeLoadingSpinner = () => {
 async function getQuote() {
     showLoadingSpinner()
     const proxyURL = 'https://cors-anywhere.herokuapp.com/'
-    const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=ru&format=json'
+    const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=eng&format=json'
     try {
         const response = await fetch(proxyURL + apiURL)
         const data = await response.json()
